c757405a05d17acd5aa7cd48caa9c788ab23900b
Nilesh_iOS <nfasate@delaplex.in>
2018-02-20 11:12:51 +0530

    Initial Commit

---
+++ SlackChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlackChat
+//
+//  Created by NILESH_iOS on 20/02/18.
+//  Copyright © 2018 iDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
